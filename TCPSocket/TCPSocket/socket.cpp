2fe13530653ef9a8c2eea3adb14768cff3d1eb17
dongmuliu <983860513@qq.com>
2019-03-05 21:07:32 +0800

    增加预处理
    增加均值滤波和畸变校正

--- TCPSocket/TCPSocket/socket.cpp
+++ TCPSocket/TCPSocket/socket.cpp
@@ -16,6 +16,18 @@ using namespace cv;
 using namespace std;
 
 #define MAXLINE 253600
+#define MAX_NUM_PIX	82656	//328 * 252
+#define LOW_AMPLITUDE 	32500
+#define MAX_PHASE        30000.0
+#define MAX_DIST_VALUE 	30000
+#define offsetPhaseDefault 0
+#define   FX  286.6034
+#define   FY  287.3857	
+#define   CX  176.2039
+#define   CY  126.5788	
+#define   K1  -0.12346
+#define   K2  0.159423
+int		drnuLut[50][252][328];
 #pragma comment (lib, "ws2_32.lib")  //加载 ws2_32.dll
 char buf[MAXLINE];
 char ptr_buf2[MAXLINE];
@@ -25,13 +37,12 @@ int scale = 1;
 int delta = 0;
 int ddepth = -1;
 int realindex, realrow, realcol;
-//vector<vector<uint16_t> >fameDepthArray;
 uint16_t fameDepthArray2[MAXLINE];
 uint16_t depth[240][320];
-//ushort raw_array[322][239];
-//ushort real_array[321][239];
 unsigned char* ptr_buf_unsigned = (unsigned char*)ptr_buf2;
-
+void imageAverageEightConnectivity(ushort *depthdata);
+void calculationAddOffset(ushort *img);
+int calculationCorrectDRNU(ushort * img);
 
 int n;
 void socket_com(char sendline[], int length)
@@ -124,13 +135,205 @@ void socket_com(char sendline[], int length)
 	//system("pause");
 
 }
+void calibrate(ushort *img)
+{
+	
+	imageAverageEightConnectivity(img);
+	
+	
+	calculationCorrectDRNU(img);
+	
+	calculationAddOffset(img);
+	
+}
+//畸变矫正
+//输入： 待矫正的图片
+//输出： 校正后的图片
+Mat undistimg(Mat src)
+{
+	
+	Mat img;
+
+	//内参矩阵
+	Mat cameraMatrix = Mat::eye(3, 3, CV_64F);		//3*3单位矩阵
+	cameraMatrix.at<double>(0, 0) = FX;
+	cameraMatrix.at<double>(0, 1) = 0;
+	cameraMatrix.at<double>(0, 2) = CX;
+	cameraMatrix.at<double>(1, 1) = FY;
+	cameraMatrix.at<double>(1, 2) = CY;
+	cameraMatrix.at<double>(2, 2) = 1;
+
+	//畸变参数
+	Mat distCoeffs = Mat::zeros(5, 1, CV_64F);		//5*1全0矩阵
+	distCoeffs.at<double>(0, 0) = K1;
+	distCoeffs.at<double>(1, 0) = K2;
+	distCoeffs.at<double>(2, 0) = 0;
+	distCoeffs.at<double>(3, 0) = 0;
+	distCoeffs.at<double>(4, 0) = 0;
+
+	Size imageSize = src.size();
+	Mat map1, map2;
+	//参数1：相机内参矩阵
+	//参数2：畸变矩阵
+	//参数3：可选输入，第一和第二相机坐标之间的旋转矩阵
+	//参数4：校正后的3X3相机矩阵
+	//参数5：无失真图像尺寸
+	//参数6：map1数据类型，CV_32FC1或CV_16SC2
+	//参数7、8：输出X/Y坐标重映射参数
+	cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, Mat(), cameraMatrix, imageSize, CV_32FC1, map1, map2);	//计算畸变映射
+	//参数1：畸变原始图像
+	//参数2：输出图像
+	//参数3、4：X\Y坐标重映射
+	//参数5：图像的插值方式
+	//参数6：边界填充方式
+	cv::remap(src, img, map1, map2, INTER_LINEAR);																	//畸变矫正
+	return img.clone();
+}
+//均值滤波
+void imageAverageEightConnectivity(ushort *depthdata)
+{
+	
+		int pixelCounter;
+		int nCols = 320;
+		int nRowsPerHalf = 120;
+		//int size = nCols * nRowsPerHalf * 2;
+		int size = 320 * 240;
+		ushort actualFrame[MAX_NUM_PIX];
+		int i, j, index;
+		int pixdata;
+		memcpy(actualFrame, depthdata, size * sizeof(ushort));
+		// up side
+		// dowm side
+		// left side and right side
+		// normal part
+		for (i = 1; i < 239; i++) {
+			for (j = 1; j < 319; j++){
+				index = i * 320 + j;
+				pixelCounter = 0;
+				pixdata = 0;
+				if (actualFrame[index] < 30000) {
+					pixelCounter++;
+					pixdata += actualFrame[index];
+				}
+				if (actualFrame[index - 1]  < 30000) {   // left
+					pixdata += actualFrame[index - 1];
+					pixelCounter++;
+				}
+				if (actualFrame[index + 1]  < 30000) {   // right
+					pixdata += actualFrame[index + 1];
+					pixelCounter++;
+				}
+				if (actualFrame[index - 321]  < 30000) {   // left up
+					pixdata += actualFrame[index - 321];
+					pixelCounter++;
+				}
+				if (actualFrame[index - 320]  < 30000) {   // up
+					pixdata += actualFrame[index - 320];
+					pixelCounter++;
+				}
+				if (actualFrame[index - 319]  < 30000) {   // right up
+					pixdata += actualFrame[index - 319];
+					pixelCounter++;
+				}
+				if (actualFrame[index + 319]  < 30000) {   // left down
+					pixdata += actualFrame[index - 321];
+					pixelCounter++;
+				}
+				if (actualFrame[index + 320]  < 30000) {   // down
+					pixdata += actualFrame[index - 320];
+					pixelCounter++;
+				}
+				if (actualFrame[index + 321]  < 30000) {   // right down
+					pixdata += actualFrame[index - 319];
+					pixelCounter++;
+				}
+
+				if (pixelCounter < 6) {
+					*(depthdata + index) = LOW_AMPLITUDE;
+				}
+				else {
+					*(depthdata + index) = pixdata / pixelCounter;
+				}
+			}
+		}
+		//
+		//end part
+
+}
+//温度校正
+int calculationCorrectDRNU(ushort * img)
+{
+	//int i, x, y, l;
+	//double dist, tempDiff = 0;
+	////printf("gOffsetDRNU = %d\n", offset);  //w
+	//uint16_t maxDistanceMM = 150000000 / 12000;
+	//double stepLSB = gStepCalMM * MAX_PHASE / maxDistanceMM;
+	////printf("stepLSB = %2.4f\n", stepLSB);  //w
+
+	//uint16_t *pMem = img;
+	//int width = 320;
+	//int height = 240;
+	//tempDiff = realTempChip - gTempCal_Temp;
+	//for (l = 0, y = 0; y< height; y++){
+	//	for (x = 0; x < width; x++, l++){
+	//		dist = pMem[l];
+
+	//		if (dist < LOW_AMPLITUDE){	//correct if not saturated
+	//			
+	//			if (dist<0)	//if phase jump
+	//				dist += MAX_DIST_VALUE;
+
+	//			i = (int)(round(dist / stepLSB));
+
+	//			if (i<0) i = 0;
+	//			else if (i >= 50) i = 49;
+
+	//			dist = (double)pMem[l] - drnuLut[i][y][x];
+	//			/*if (l == 1300){
+	//			printf(" DRNULutPixel = %d,", drnuLut[i][y][x]);
+	//			}
+	//			if (l == 1301){
+	//			printf("  %d,", drnuLut[i][y][x]);
+	//			}
+	//			if (l == 1302){
+	//			printf(" %d \n", drnuLut[i][y][x]);
+	//			}*/
+	//			// temprature cali
+	//			dist -= tempDiff * 3.12262;	// 0.312262 = 15.6131 / 50
+
+	//			pMem[l] = (uint16_t)round(dist);
+
+	//		}	//end if LOW_AMPLITUDE
+	//	}	//end width
+	//}	//end height
+	////printf(" pMem = %d, %d, %d\n", pMem[1300], pMem[1301], pMem[1302]);
+	return 0;
+
+}
+//深度补偿
+void calculationAddOffset(ushort *img)
+{
+	int offset = 0;
+	uint16_t maxDistanceCM = 0;
+	int l = 0;
+	//offsetPhase = MAX_PHASE / (gSpeedOfLightDiv2 / configGetModulationFrequency(deviceAddress) / 10) * value;	
+	offset = offsetPhaseDefault;
+	//printf("offset 1 = %d\n", offset);
+	uint16_t val;
+	uint16_t *pMem = img;
+	int numPix = 320 * 240;
+
+	for (l = 0; l<numPix; l++){
+		val = pMem[l];
+		if (val < 30000) { //if not low amplitude, not saturated and not  adc overflow
+			pMem[l] = (val + offset) % MAX_DIST_VALUE;
+		}
+	}
+}
 int main()
 {
 
-	Mat img;
-	Mat img_threshold;
-	Mat img_gray;
-	Mat img_roi;
+	
 
 	int count = 0;
 	double hu[7];
@@ -151,6 +354,8 @@ int main()
 
 			//cout << fameDepthArray2[j] << " ";
 		}
+		//滤波
+		calibrate(fameDepthArray2);
 		for (int i = 0; i < 240; i++)
 		{
 			for (int j = 0; j < 320; j++)
@@ -160,7 +365,7 @@ int main()
 		}
 
 		Mat src_1(240, 320, CV_16UC1, Scalar(0));
-		Mat src_2(240, 320, CV_8UC1, Scalar(0));
+		Mat imshowsrc(240, 320, CV_8UC1, Scalar(0));
 		for (int i = 0; i < 240; i++)
 		{
 			for (int j = 0; j < 320; j++)
@@ -170,19 +375,21 @@ int main()
 			}
 
 		}
-
+		//畸变矫正
+	 Mat src_2=	undistimg(src_1);
+		
 		for (int i = 0; i < 240; i++)
 		{
 			for (int j = 0; j < 320; j++)
 			{
-				src_2.at<uchar>(i, j) = 255 - src_1.at<ushort>(i, j) * 25 / 3000;
+				imshowsrc.at<uchar>(i, j) = 255 - src_2.at<ushort>(i, j) * 25 / 3000;
 				//cout << src2.at<uchar>(i, j) << endl;
 			}
 
 		}
 
 
-		imshow("src2", src_2);
+		imshow("test", imshowsrc);
 		waitKey(1);
 	}
 	return 0;
